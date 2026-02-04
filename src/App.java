8b0cd1f0d161e9571c3a11eb630104a8ec945245
Sanni <117445321+fsannicolo@users.noreply.github.com>
2026-02-04 20:14:38 +0100

    Clean up

--- src/App.java
+++ src/App.java
@@ -2,7 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         DoublyLinkedList dl1 = new DoublyLinkedList();
-        DoublyLinkedList dl2 = new DoublyLinkedList();
 
         Node n1 = new Node('A');
         Node n2 = new Node('B');
@@ -15,11 +14,6 @@ public class App {
         dl1.add(n3);
         dl1.print(true);
         dl1.print(false);
-
-        dl2.add(n4);
-        dl2.add(n5);
-        dl1.merge(dl2);
-        dl1.print(true);
 
         // and so on...
     }
